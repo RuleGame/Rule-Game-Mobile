7b88b09fec2aa1898163f785515af7dad2994c93
Kevin Mui <kmui2@wisc.edu>
2021-02-15 01:28:24 -0600

    :sparkles: Add showGridMemoryOrder functionality

--- lib/widgets/piece.dart
+++ lib/widgets/piece.dart
@@ -18,36 +18,51 @@ class Piece extends HookWidget {
     final boardStore = Provider.of<BoardStore>(context);
 
     return Container(
-      child: Stack(
-        fit: StackFit.expand,
-        children: [
-          Observer(
-            builder: (_) {
-              // Put isPaused observable inside to avoid the no observable found
-              // warning
-              final canDrag =
-                  !boardStore.isPaused && boardObject.buckets.isNotEmpty;
-              return Draggable(
-                maxSimultaneousDrags: canDrag ? 1 : 0,
-                data: boardObject,
-                child:
-                    Shape(shape: boardObject.shape, color: boardObject.color),
-                feedback: Container(
-                    child: Transform.translate(
-                      offset: Offset(-75, -75),
-                      child: Shape(
-                        shape: boardObject.shape,
-                        color: boardObject.color,
-                      ),
-                    ),
-                    width: 100,
-                    height: 100),
-                dragAnchor: DragAnchor.pointer,
-              );
-            },
-          ),
-          if (boardObject.buckets.isEmpty) Shape(shape: SpecialShape.CLOSE),
-        ],
+      child: Observer(
+        builder: (_) => Stack(
+          // fit: StackFit.expand,
+          children: [
+            Positioned.fill(
+              child: Observer(
+                builder: (_) {
+                  // Put isPaused observable inside to avoid the no observable found
+                  // warning
+                  final canDrag =
+                      !boardStore.isPaused && boardObject.buckets.isNotEmpty;
+                  return Draggable(
+                    maxSimultaneousDrags: canDrag ? 1 : 0,
+                    data: boardObject,
+                    child: Shape(
+                        shape: boardObject.shape, color: boardObject.color),
+                    feedback: Container(
+                        child: Transform.translate(
+                          offset: Offset(-75, -75),
+                          child: Shape(
+                            shape: boardObject.shape,
+                            color: boardObject.color,
+                          ),
+                        ),
+                        width: 100,
+                        height: 100),
+                    dragAnchor: DragAnchor.pointer,
+                  );
+                },
+              ),
+            ),
+            if (boardObject.buckets.isEmpty)
+              Positioned.fill(child: Shape(shape: SpecialShape.CLOSE)),
+            if (boardStore.showGridMemoryOrder)
+              Container(
+                padding: EdgeInsets.all(0.5),
+                decoration: BoxDecoration(
+                  border: Border.all(),
+                  color: Colors.white,
+                  borderRadius: BorderRadius.all(Radius.circular(20)),
+                ),
+                child: Text('${boardStore.getMoveNum(boardObject.id)}'),
+              ),
+          ],
+        ),
       ),
       constraints: BoxConstraints.expand(),
     );
