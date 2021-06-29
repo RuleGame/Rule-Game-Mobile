6ce4aef36c04ce446055d1054a892c1c2f3aff83
Kevin Mui <kmui2@wisc.edu>
2021-06-29 18:42:35 -0500

    :alien: Update network permissions and use Sapir

--- lib/utils/api.dart
+++ lib/utils/api.dart
@@ -3,9 +3,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:rulegamemobile/utils/models.dart';
 
-// TODO: Use Sapir
-// final API_HOST_ORIGIN = '192.168.1.9:7150';
-final API_HOST_ORIGIN = '34.135.29.63';
+final API_HOST_ORIGIN = 'sapir.psych.wisc.edu:7150';
 final BASE_ROUTE = '/w2020-dev';
 
 Future<GetColorMapResBody> getColorMapApi() async =>
